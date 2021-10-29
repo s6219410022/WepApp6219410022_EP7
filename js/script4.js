b7e2899f27a2ef5239cbae0c4f7917c7c6d5d20a
s6219410022 <s6219410022@sau.ac.th>
2021-10-29 13:38:05 +0700

    Code V.1 : Do xxxx....

---
+++ js/script4.js
@@ -0,0 +1,14 @@
+console.log(7 > 9)
+console.log(7 != 9)
+
+console.log(99==99)
+console.log("99" == "99")
+console.log("99" == 99)
+console.log("99" === 99)
+
+
+
+console.log("99" != 99)
+console.log("99" !== 99)
+
+
